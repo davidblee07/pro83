c8f2a7b18d974c7b2d04fcde470913c387c92511
davidblee07 <93348233+davidblee07@users.noreply.github.com>
2023-04-13 19:10:23 -0300

    Add files via upload

---
+++ screens/PostCard.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default class PostCard extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SafeAreaView style={styles.droidSafeArea} />
+        <View style={styles.cardContainer}>
+          <View style={styles.authorContainer}>
+            <View style={authorImageContainer}>
+              <IMage
+                source={require("../assets/profile_img.png")}
+                Style={styles.profileImage}
+              ></IMage>
+            </View>
+            <View style={authorNameContainer}>
+              <Text style={styles.authorNameText}>
+                {this.props.post.author}
+              </Text>
+            </View>
+          </View>
+          <Image
+            style={styles.postImage}
+            source={require("../assets/post.jpeg")}
+          />
+          <View styçle={styles.captionContainer}>
+            <Text style={styles.captionText}>{this.props.post.caption}</Text>
+          </View>
+          <View style={styles.actionContainer}>
+            <View Style={styles.likeButton}>
+              <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+              <Text style={styles.likeText}>12k</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  cardContainer: {
+    margin: RFValue(13),
+    backgroundColor: "#2a2a2a",
+    borderRadius: RFValue(20),
+    padding: RFValue(20),
+  },
+  authorContainer: {
+    flex: 0.1,
+    flexDirection: "row",
+  },
+  authorImageContainer: {
+    flex: 0.15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+    borderRadius: RFValue(100),
+  },
+  authorNameContainer: {
+    flex: 0.85,
+    justifyContent: "center",
+  },
+  authorNameText: {
+    color: "white",
+    fontSize: RFValue(20),
+  },
+  postImage: {
+    marginTop: RFValue(20),
+    resizeMode: "contain",
+    width: "100%",
+    alignSelf: "center",
+    height: RFValue(275),
+  },
+  captionContainer: {},
+  captionText: {
+    fontSize: 13,
+    color: "white",
+    paddingTop: RFValue(10),
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: RFValue(10),
+  },
+  likeButton: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#eb3948",
+    borderRadius: RFValue(30),
+  },
+  likeText: {
+    color: "white",
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5),
+  },
+});
